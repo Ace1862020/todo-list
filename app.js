f63850cb5e4f46f446a93ec3286f598593f2cc1d
Ace1862020 <kyds1862015@gmail.com>
2021-01-17 20:27:00 +0800

    feat:add edit function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const db = mongoose.connection
 
 const Todo = require('./models/todo')
+const { findById } = require('./models/todo')
 
 const app = express() // 2呼叫 express() 來啟動應用程式伺服器
 
@@ -42,6 +43,36 @@ app.post('/todos', (req, res) => {
   const name = req.body.name
   return Todo.create({ name })
     .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Detail Page
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+//Edit Page
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
